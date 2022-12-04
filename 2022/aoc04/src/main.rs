a43921ce25533197e5a80f21883f1a1bb968ce16
annietse <annietse03@gmail.com>
2022-12-04 20:51:55 +0000

    Part2 of aoc04

--- 2022/aoc04/src/main.rs
+++ 2022/aoc04/src/main.rs
@@ -3,10 +3,6 @@ use std::io::{self, prelude::*, BufReader};
 use std::path::Path;
 
 fn main() {
-    part1()
-}
-
-fn part1() {
     let mut count: u32 = 0;
     if let Ok(lines) = read_lines("./hosts") {
         for line in lines {
@@ -22,19 +18,30 @@ fn part1() {
                         limits.push(limit);
                     }
                 }
-                println!("{:?}", limits);
-                if limits[0] <= limits[2] && limits[1] >= limits[3]
-                    || limits[0] >= limits[2] && limits[1] <= limits[3]
-                {
-                    count += 1;
-                }
+                // println!("{:?}", limits);
+                // count = part1(limits, count);
+                count = part2(limits, count);
             }
         }
     }
     println!("Count: {}", count);
 }
 
+fn part1(limits: Vec<u32>, mut count: u32) -> u32 {
+    if limits[0] <= limits[2] && limits[1] >= limits[3]
+        || limits[0] >= limits[2] && limits[1] <= limits[3]
+    {
+        count += 1;
+    }
+    return count;   
+}
 
+fn part2(limits: Vec<u32>, mut count: u32) -> u32 {
+    if !(limits[0] > limits[3] || limits[1] < limits[2]) {
+        count += 1;
+    }
+    return count;
+}
 
 fn read_lines<P>(input: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
