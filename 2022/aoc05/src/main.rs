0e8b4a0d3cdb765e5dfdb43a3fdd5b66bf97ffaa
annietse <annietse03@gmail.com>
2022-12-05 10:49:26 +0000

    Part2 of aoc05

--- 2022/aoc05/src/main.rs
+++ 2022/aoc05/src/main.rs
@@ -3,7 +3,8 @@ use std::io::{self, prelude::*};
 use std::path::Path;
 
 fn main() {
-    part1()
+    // part1()
+    part2()
 }
 
 fn part1() {
@@ -39,6 +40,59 @@ fn part1() {
                         array[destination].push(*temp);
                         array[location].pop();
                     }
+                }
+                // while no numbers found
+            }
+        }
+    }
+    let mut result: Vec<char> = Vec::new();
+    for item in array {
+        result.push(*item.last().unwrap());
+    }
+    println!("{:?}",result);
+    
+}
+
+fn part2() {
+    let mut array: [Vec<char>; 9] = Default::default();
+    let mut array_complete = false;
+    if let Ok(lines) = read_lines("./hosts") {
+        for line in lines {
+            if let Ok(ip) = line {
+                // create vec
+                let v: Vec<char> = ip.chars().collect();
+                if v.len() != 0 && v[1] == '1'{
+                    array_complete = true;
+                }
+                if array_complete == false {
+                    for n in 1..ip.chars().count() {
+                        if v[n] != ' '&& v[n] != '['&& v[n] != ']' {
+                            // println!("{} {}",v[n], (n+2)/4);
+                            let index = (n+2)/4;
+                            array[index].insert(0, v[n]);
+                            // println!("{:?}", array[index]);
+                        }
+                        // println!("{:?}", array[index]);
+                    }
+                    
+                }
+                if v.len() != 0 && v[0] == 'm' {
+                    let instruction: Vec<&str> = ip.split(" ").collect();
+                    let count = instruction[1].parse::<usize>().unwrap();
+                    let location = instruction[3].parse::<usize>().unwrap()-1;
+                    let destination = instruction[5].parse::<usize>().unwrap()-1;
+                    // for _i in 0..count {
+                    //     let temp = array[location].last().unwrap();
+                    //     array[destination].push(*temp);
+                    //     array[location].pop();
+                    // }
+                    let mut tempVec: Vec<char> = Vec::new();
+                    for _i in 0..count {
+                        let temp = array[location].last().unwrap();
+                        tempVec.insert(0, *temp);
+                        array[location].pop();
+                    }
+                    array[destination].extend(tempVec);
                 }
                 // while no numbers found
             }
