d42d8973749a7e70c7c6e69667eab6a53c6e4f7a
annietse <annietse03@gmail.com>
2022-12-06 13:17:14 +0000

    Part1 aoc06

---
+++ 2022/aoc06/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    part1(contents);
+}
+
+fn part1(contents: String) {
+    // make empty set
+    let mut set: HashSet<char> = HashSet::new();
+    for (i, c) in contents.chars().enumerate() {
+        if set.len() < 4 {
+            if set.contains(&c) {
+                set.clear();
+            }
+            set.insert(c);
+        } else {
+            println!("{}", i);
+            break;
+        }
+    }
+}
